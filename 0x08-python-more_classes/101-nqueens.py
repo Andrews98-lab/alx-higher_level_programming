d605dbf79f855ccf9cebf8ad650b0a1e9c8862ed
Andrews <baidooandrews98@gmail.com>
2023-04-03 13:23:56 -0700

    all task

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,53 +1,44 @@
 #!/usr/bin/python3
-"""
-N Queens problem solver
-"""
 
 import sys
 
-
-def is_valid(board, row, col):
-    """
-    Check if it's valid to place a queen at board[row][col]
-    """
-    # Check this row on the left side
+def is_safe(board, row, col, n):
+    # Check if there is a queen in the same row
     for i in range(col):
-        if board[row][i]:
+        if board[row][i] == 1:
             return False
 
-    # Check upper diagonal on left side
+    # Check if there is a queen in the upper diagonal
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j]:
+        if board[i][j] == 1:
             return False
 
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j]:
+    # Check if there is a queen in the lower diagonal
+    for i, j in zip(range(row, n), range(col, -1, -1)):
+        if board[i][j] == 1:
             return False
 
     return True
 
-
-def solve(board, col, solutions):
-    """
-    Recursive function to solve the problem
-    """
-    if col >= N:
-        # Found a solution
+def solve_n_queens(board, col, n):
+    if col == n:
+        # Print the solution
         solution = []
-        for i in range(N):
-            for j in range(N):
-                if board[i][j]:
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
                     solution.append([i, j])
-        solutions.append(solution)
-        return
+        print(solution)
+        return True
 
-    for row in range(N):
-        if is_valid(board, row, col):
-            board[row][col] = 1
-            solve(board, col + 1, solutions)
-            board[row][col] = 0
+    res = False
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            res = solve_n_queens(board, col + 1, n) or res
+            board[i][col] = 0
 
+    return res
 
 if __name__ == "__main__":
     # Check if the correct number of arguments is provided
@@ -55,24 +46,21 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         sys.exit(1)
 
+    # Get the value of N
     try:
-        N = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
-    if N < 4:
+    # Check if N is at least 4
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
     # Initialize the board
-    board = [[0] * N for _ in range(N)]
+    board = [[0 for x in range(n)] for y in range(n)]
 
     # Solve the problem
-    solutions = []
-    solve(board, 0, solutions)
-
-    # Print solutions
-    for solution in solutions:
-        print(solution)
+    solve_n_queens(board, 0, n)
 
